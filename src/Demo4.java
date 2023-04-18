b3be9ae55df68c6205559787bf5183237ca6963b
smita <smitthecoder@gmail.com>
2023-04-18 23:21:25 +0530

    Commit X to branch1

--- src/Demo4.java
+++ src/Demo4.java
@@ -5,7 +5,8 @@ public class Demo4 {
 	}
 	public static void main(int a) {
 		System.out.println("Created a new branch in GIT and working on it!!");
-		System.out.println("Thank you");
+		System.out.println("Thank you so much");
+		System.out.println("I've made some changes here!");
 	}
 }
 
