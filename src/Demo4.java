129ee175cc9cf8ab5ea321d614d989a6cda6d4b3
smita <smitthecoder@gmail.com>
2023-04-18 22:55:47 +0530

    Commit X

--- src/Demo4.java
+++ src/Demo4.java
@@ -3,5 +3,8 @@ public class Demo4 {
 	public static void main(String[] args) {
 		System.out.println("Created a new branch in GIT and working on it!!");
 	}
+	public static void main(int a) {
+		System.out.println("Created a new branch in GIT and working on it!!");
+	}
 }
 
