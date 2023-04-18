aaf1fdc51c44763476b38d7ffe4c005e9b28a393
smita <smitthecoder@gmail.com>
2023-04-19 00:26:43 +0530

    message

--- src/Demo4.java
+++ src/Demo4.java
@@ -12,5 +12,8 @@ public class Demo4 {
 	public static void main(float b) {
 		System.out.println("created a new method here");
 	}
+	public static void main(String c) {
+		System.out.println("String c");
+	}
 }
 
