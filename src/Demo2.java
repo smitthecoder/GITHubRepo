c2334fcf9395cf9b0b058173e82cdde0e8507cd9
smita <smitthecoder@gmail.com>
2023-04-18 11:38:48 +0530

    Added new file

---
+++ src/Demo2.java
@@ -0,0 +1,9 @@
+
+public class Demo2 {
+	public static void main(String[] args) {
+		System.out.println("First main method");
+	}
+	public static void main() {
+		System.out.println("Second: overloaded main method");
+	}
+}
