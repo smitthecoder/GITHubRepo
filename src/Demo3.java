d0876ef980a2f0ed7d504f90e298bdd3a9fe057d
smita <smitthecoder@gmail.com>
2023-04-18 11:50:41 +0530

    commit

---
+++ src/Demo3.java
@@ -0,0 +1,12 @@
+
+public class Demo2 {
+	public static void main(String[] args) {
+		System.out.println("First main method");
+	}
+	public static void main() {
+		System.out.println("Second: overloaded main method");
+	}
+	public static void main(int a) {
+		System.out.println("Third: overloaded main method");
+	}
+}
